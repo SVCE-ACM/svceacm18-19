2f56d49028ef8488fbf7a3ef3344bde03bb6e3db
Yuvaraj Jayavel <jyuvaraj03@gmail.com>
2018-12-01 00:54:24 +0530

    Javascript code cleanup

---
+++ js/main.js
@@ -0,0 +1,45 @@
+function outroAnimation() {
+	$('.nav-link').click(function() {
+        var link = this.href;
+        console.log(link);
+        if ($(this).hasClass('active')) {
+            return;
+        } else {
+            event.preventDefault();
+            $('.navbar').addClass('fadeOutUp');
+            $('.wrapper').addClass('fadeOut');
+            setTimeout(function() {
+                window.location.href = link;
+            }, 500);
+        }
+    });
+}
+
+function makeParallax(windowWidth, padding) {
+    if (windowWidth <= 1024) {
+        $('.parallax-bg').removeClass('parallax-bg');
+        $('.page-content').css({
+            'margin-top': '0'
+        });
+    } else {
+        $('.page-head').addClass('parallax-bg');
+        $('.page-content').css({
+            'margin-top': (($('.test').height() + 60 + padding) + 'px')
+        });
+    }
+}
+
+function switchTabs(tabType) {
+    $(".tabs").on("click", "a", function() {
+        $('.tabs a').removeClass("active");
+        $(this).addClass('active');
+        var tab_id = this.href.substring(this.href.indexOf("#") + 1);
+        $(tabType).removeClass('active');
+        $('#' + tab_id).addClass('active');
+        // $(window).scrollTop($('#' + tab_id).offset().top + 60);
+
+        $('html, body').animate({
+            scrollTop: $('#' + tab_id).offset().top - 70
+        }, 1000);
+    });
+}
